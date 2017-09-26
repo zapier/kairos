d11723f81c2533a764f36c9716e7494f4cadcd0d
Claudiu Popa <pcmanticore@gmail.com>
2017-09-26 07:46:01 +0200

    Bump local version

--- kairos/__init__.py
+++ kairos/__init__.py
@@ -4,7 +4,7 @@ Copyright (c) 2012-2017, Agora Games, LLC All rights reserved.
 https://github.com/agoragames/kairos/blob/master/LICENSE.txt
 '''
 from __future__ import absolute_import
-__version__ = "0.10.1"
+__version__ = "0.10.1.zapier"
 
 from .timeseries import Timeseries
 from .exceptions import *
